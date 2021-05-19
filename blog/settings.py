a26b238133de3f9fd85d4acc37644a4830035c57
unknown <akbari.adrian@gmail.com>
2021-05-19 13:07:28 +0200

    tinymce changed config to basic

--- blog/settings.py
+++ blog/settings.py
@@ -140,7 +140,7 @@ TINYMCE_DEFAULT_CONFIG = {
     'custom_undo_redo_levels': 20,
     'selector': 'textarea.tinymcewidget',
     'plugins': 'advlist,autolink,lists,link,image,charmap,print,preview,anchor,searchreplace,visualblocks,code,fullscreen,insertdatetime,media,table,paste,code,help,wordcount'
-    'toolbar': 'undo redo | formatselect | ' 'bold italic backcolor | alignleft aligncenter ' 'alignright alignjustify | bullist numlist outdent indent | ' 'removeformat | help',
+    'toolbar': 'undo redo | formatselect | bold italic backcolor | alignleft aligncenter alignright alignjustify | bullist numlist outdent indent | removeformat | help',
     'contextmenu': 'formats | link image',
     'menubar': False,
     'statusbar': True,
