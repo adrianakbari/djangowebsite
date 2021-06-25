a2cbf2cf9788c4327f9548dc67cb48c3432f2b3c
unknown <akbari.adrian@gmail.com>
2021-06-25 13:28:16 +0200

    production security applied

--- blog/settings.py
+++ blog/settings.py
@@ -8,12 +8,15 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/2.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'k*b$$!%hiswnhp2pad)6(u4x_g)r5w%@&zlv&@2hrz4#f3+#57'
+# SECRET_KEY = 'k*b$$!%hiswnhp2pad)6(u4x_g)r5w%@&zlv&@2hrz4#f3+#57'
+SECRET_KEY = os.environ.get(
+    'DJANGO_SECRET_KEY', 'k*b$$!%hiswnhp2pad)6(u4x_g)r5w%@&zlv&@2hrz4#f3+#57')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+# DEBUG = False
+DEBUG = os.environ.get('DJANGO_DEBUG', '') != 'False'
 
-ALLOWED_HOSTS = ['shielded-temple-04378.herokuapp.com']
+ALLOWED_HOSTS = ['shielded-temple-04378.herokuapp.com', 'adrianakbari.com']
 
 
 # Application definition
