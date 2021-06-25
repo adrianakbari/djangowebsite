d45a0dcf1775f4738081b98405fd0cf84064bfb6
unknown <akbari.adrian@gmail.com>
2021-06-25 13:37:20 +0200

    debug is false now

--- blog/settings.py
+++ blog/settings.py
@@ -14,7 +14,7 @@ SECRET_KEY = os.environ.get(
 
 # SECURITY WARNING: don't run with debug turned on in production!
 # DEBUG = False
-DEBUG = os.environ.get('DJANGO_DEBUG', '') != 'False'
+DEBUG = os.environ.get('DJANGO_DEBUG', '') != 'True'
 
 ALLOWED_HOSTS = ['shielded-temple-04378.herokuapp.com', 'adrianakbari.com']
 
